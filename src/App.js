b964cbd7c69fc7ba6ed2b49a3a6639083ec3e4db
ToeWaiPhyo <toewaiphyo@ucstgi.edu.mm>
2022-02-20 12:35:40 +0630

    reactstarttesting

--- src/App.js
+++ src/App.js
@@ -2,22 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstname="John";
+  const lastname="Wick";
+  const age=28;
+  const job="React Developer";
+  const getFullName=(firstname,lastname)=>{return `${firstname} ${lastname}`}
+  const hinttext="Enter Your Details";
+  const inputbox=<input placeholder={hinttext} autoComplete/>;
+  const arr=['apple','orange','banana','papaya','coconut'];
+  const obj={
+    name:"toe wai phyo",
+    age:28,
+    hobby:'coding'
+  };
+  const obj2={
+    title:"React Js",
+    description:"  React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study"
+  }
+  const design={
+    margin:'16px',
+    borderRadius:'5px',
+    boxShadow:'0 2px 5px #ccc',
+    backgroundColor: '#888888',
+    fontSize:'18px',
+    alignContent:'flex-start',
+    padding:'5px'
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Full Name:{firstname+" "+lastname}</h3>
+      <p>My name is:{getFullName(firstname,lastname)}</p>
+      <p>Job: {job}</p>
+      <p>Age : {age}</p>
+      {inputbox}<br></br>
+      {arr.length}<br/>
+      {arr.splice(2,1)}<br/>
+      {arr.push('banana')}<br/>
+      {arr}<br></br>
+      {obj.name}<br/>
+      {obj.age}<br/>
+      {obj.hobby}<br/>
+      <div style={design}>
+        <h3>{obj2.title}</h3>
+        <p>{obj2.description}</p>
+      </div>
+      <div style={design}>
+        <h3>{obj2.title}</h3>
+        <p>{obj2.description}</p>
+      </div>
+      <hr></hr>
+      <div style={design}>
+        <h3>{obj2.title}</h3>
+        <p>{obj2.description}</p>
+      </div>
+      <hr></hr>
+      
+    
+
+      
     </div>
   );
 }
