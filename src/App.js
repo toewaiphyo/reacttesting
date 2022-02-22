50c58ab6069050a78a839373e03bab3c3de9d232
ToeWaiPhyo <toewaiphyo@ucstgi.edu.mm>
2022-02-22 15:42:26 +0630

    component in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 
+import React,{Component} from 'react';
+import Card from './card.js';
 import './App.css';
-import Card from './card';
-
-function App() {
-  const firstname="John";
-  const lastname="Wick";
-  const age=28;
-  const job="React Developer";
-  const getFullName=(firstname,lastname)=>{return `${firstname} ${lastname}`}
-  const hinttext="Enter Your Details";
-  const inputbox=<input placeholder={hinttext} autoComplete/>;
-  const arr=['apple','orange','banana','papaya','coconut'];
-  const obj={
-    name:"toe wai phyo",
-    age:28,
-    hobby:'coding'
-  };
-  const obj2={
-    title:"React Js",
-    description:"  React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study"
+class App extends Component{
+  state={
+    showdialog:true
   }
-  const arrh=[
+  // const firstname="John";
+  // const lastname="Wick";
+  // const age=28;
+  // const job="React Developer";
+  // const getFullName=(firstname,lastname)=>{return `${firstname} ${lastname}`}
+  // const hinttext="Enter Your Details";
+  // const inputbox=<input placeholder={hinttext} autoComplete/>;
+  // const arr=['apple','orange','banana','papaya','coconut'];
+  // const obj={
+  //   name:"toe wai phyo",
+  //   age:28,
+  //   hobby:'coding'
+  // };
+  // const obj2={
+  //   title:"React Js",
+  //   description:"  React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study React Js is a good one to study and i am also love to study"
+  // }
+  arrh=[
       {
         id:1,
         title:"React Js",
@@ -38,48 +41,37 @@ function App() {
       }
   
     ]
-    const card=arrh.map((item,pos)=>{
+    
+    card=this.arrh.map((item,pos)=>{
       console.log(item);
       return(
           <Card key={pos} title={item.title} description={item.description} id={item.id}/>
       )
     });
+    onhideButtonClick=()=>{
+      // let updatestate=!this.state.showdialog;
+      // this.setState({
+      //   showdialog:updatestate
+      // })
+      this.setState((prevState,prevProps)=>{
+        return{showdialog:!prevState.showdialog}
+      })
+    }
   
 
  
-  return (
-    <div className="App">
-      <h3>Full Name:{firstname+" "+lastname}</h3>
-      <p>My name is:{getFullName(firstname,lastname)}</p>
-      <p>Job: {job}</p>
-      <p>Age : {age}</p>
-      {inputbox}<br></br>
-      {arr.length}<br/>
-      {arr.splice(2,1)}<br/>
-      {arr.push('banana')}<br/>
-      {arr}<br></br>
-      {obj.name}<br/>
-      {obj.age}<br/>
-      {obj.hobby}<br/>
-      {/* <div className='tested'>
-        <h3>{obj2.title}</h3>
-        <p>{obj2.description}</p>
+  render(){
+    return (
+      <div className="App">
+        <button onClick={this.onhideButtonClick}>{this.state.showdialog ? 'Hide blog':'Show blog'}</button>
+        <br></br> 
+        {
+        this.state.showdialog?this.card:null
+        }         
       </div>
-      <div className='tested'>
-        <h3>{obj2.title}</h3>
-        <p>{obj2.description}</p>
-      </div>
-      <div className='tested'>
-        <h3>{obj2.title}</h3>
-        <p>{obj2.description}</p>
-      </div> */}
-      {card}
+    );
+  }
+  }
       
-    
-
-      
-    </div>
-  );
-}
 
 export default App;
--- src/card.js
+++ src/card.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './App.css';
+import classes from './card.module.css';
 
 const Card=(props)=>{
     return(
-    <div className='tested'>
+    <div className={classes.cards}>
     <h3>{props.title}</h3>
     <p>{props.description}</p>
   </div>)
